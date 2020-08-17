779345094023bdc659d27f614b5f2388c7617752
taes-k <cobura12@gmail.com>
2020-08-17 15:03:40 +0900

    feature: r2dbc example detail

--- spring-webflux-r2dbc/src/main/java/com/taes/r2dbc/service/UserService.java
+++ spring-webflux-r2dbc/src/main/java/com/taes/r2dbc/service/UserService.java
@@ -1,7 +1,23 @@
 package com.taes.r2dbc.service;
 
+import com.taes.r2dbc.entity.User;
+import com.taes.r2dbc.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Flux<User> getAllUser()
+    {
+        return userRepository.findAll();
+    }
 }
